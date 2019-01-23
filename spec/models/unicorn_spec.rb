e18b90fc5267e37ff0209c051c044a2b4037daa9
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-22 17:37:39 -0700

    fix

--- spec/models/unicorn_spec.rb
+++ spec/models/unicorn_spec.rb
@@ -1,7 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Unicorn, :type => :model do
-  it "orders them in reverse chronologically" do
+
+  it "Validations" do
+    validates_presence_of :name
+    validates_presence_of :color
+  end
+
+  it ".is_white?" do
     ornot = Unicorn.create(name: "Ornot", color: "white")
     howsat = Unicorn.create(name: "Howsat", color: "grey")
     
