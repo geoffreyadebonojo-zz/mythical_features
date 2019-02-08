9b102b37703c3135b1ccbef917c13b9dc71ac300
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-07 19:53:05 -0700

    update test

--- spec/models/unicorn_spec.rb
+++ spec/models/unicorn_spec.rb
@@ -2,15 +2,17 @@ require "rails_helper"
 
 RSpec.describe Unicorn, :type => :model do
 
-  it "Validations" do
-    validates_presence_of :name
-    validates_presence_of :color
+  it "Attributes" do
+    ornot = Unicorn.create(name: "Ornot", color: "white")
+
+    expect(ornot.name).to eq("Ornot")
+    expect(ornot.color).to eq("white")
   end
 
   it ".is_white?" do
     ornot = Unicorn.create(name: "Ornot", color: "white")
     howsat = Unicorn.create(name: "Howsat", color: "grey")
-    
+
     expect(ornot.is_white?).to eq(true)
     expect(howsat.is_white?).to eq(false)
   end
