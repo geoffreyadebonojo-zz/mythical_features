df0286c4cf68ab02b2343062e3274a1969b802c1
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-07 20:00:16 -0700

    remove structure

--- app/models/unicorn.rb
+++ app/models/unicorn.rb
@@ -1,10 +1,5 @@
 class Unicorn < ApplicationRecord
 
-  validates_presence_of :name
-  validates_presence_of :color
-
-  def is_white?
-    self.color.downcase == "white"
-  end
+  # Your Code here
 
 end
